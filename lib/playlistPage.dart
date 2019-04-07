35de945ed6b4a5afe171295f1ffd198a66d7c823
seedinwind <1211615659@qq.com>
2019-04-07 23:41:05 +0800

    Complex scroll

--- lib/playlistPage.dart
+++ lib/playlistPage.dart
@@ -13,15 +13,13 @@ class _PlaylistPageState extends State<PlaylistPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: ListView.separated(
-      itemCount: 5,
-      itemBuilder: (context, index) {
-        return ListTile(title:Text("listItem") ,);
-      },
-      separatorBuilder: (context, index) => Divider(
-            color: Colors.cyanAccent,
-            height: 10,
-          ),
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverToBoxAdapter(),
+            SliverGrid(),
+            SliverList(),
+            SliverList(),
+          ],
     ));
   }
 }
